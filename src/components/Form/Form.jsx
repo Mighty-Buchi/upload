6c246873d53b2af92c883e1c0e0757d7d1309e96
Mighty-Buchi <ihiasobuchi@gmail.com>
2025-08-05 16:36:40 +0100

    first-commit

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import './Form.css'
+
+const Form = () => {
+    return (
+        <div className='signup'>
+            <h1>Upload Files</h1>
+            <form className='box-parent'>
+                <div className="box-1">
+                    <label htmlFor="auth-key">Authentication key</label><br />
+                    <input type="password" name='auth.key' placeholder='**************************' /><br />
+
+                    <label htmlFor="title">Title</label><br />
+                    <input type="text" name='title' /><br />
+
+                    <label htmlFor="">Description</label><br />
+                    <textarea name="description" cols={30} rows={10} placeholder='Description'></textarea>
+                </div>
+                <div className="box-2">
+                    <label htmlFor="tag">Tags (comma seperated)</label><br />
+                    <input type="text" name='date' placeholder='Tags (comma seperated)' /><br />
+
+                    <label htmlFor="date">Date</label><br />
+                    <input type="date" name='date' placeholder='' /><br />
+
+                    <label htmlFor="drop-down">Software</label><br />
+                    <select name="drop-down">
+                        <option value="sketch">sketch</option>
+                        <option value="Figma">Figma</option>
+                        <option value="Framer">Framer</option>
+                    </select>
+
+                    <div className="upload">
+                        <label htmlFor="">Thumbnail image</label><br />
+                        <input type="file" />
+
+                        <label htmlFor="">Source File</label><br />
+                        <input type="file" />
+                    </div>
+                </div>
+
+                <div className="btn">
+                    <button type='submit'>Submit</button>
+                </div>
+
+            </form>
+        </div>
+    )
+}
+
+export default Form
